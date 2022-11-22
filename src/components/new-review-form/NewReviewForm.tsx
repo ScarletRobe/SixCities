90ee52887f30745704af7a5f5641c0200050fb94
Leonid Chernyakov <scrltrb@gmail.com>
2022-11-22 16:35:07 +0300

    Feat: add state

--- src/components/new-review-form/NewReviewForm.tsx
+++ src/components/new-review-form/NewReviewForm.tsx
@@ -1,51 +1,59 @@
-function NewReviewForm () {
+import { ChangeEvent } from 'react';
+import { useState } from 'react';
+import ReviewStars from './ReviewStars';
+
+const MIN_REVIEW_LENGTH = 50;
+
+type ReviewDataState = {
+  rating: null | string;
+  review: string;
+}
+
+function validateForm ({rating, review}: ReviewDataState) {
+  if (!rating) {return false;}
+  if (review.length <= MIN_REVIEW_LENGTH) {return false;}
+
+  return true;
+}
+
+function NewReviewForm (): JSX.Element {
+  const [reviewData, setReviewData] = useState<ReviewDataState>({
+    rating: null,
+    review: '',
+  });
+
+  const formChangeHandler = (evt:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = evt.target;
+    setReviewData({
+      ...reviewData,
+      [name]: value,
+    });
+  };
 
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <ReviewStars changeHandler={formChangeHandler} />
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        onChange={formChangeHandler}
+        minLength={MIN_REVIEW_LENGTH}
+      />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your
-          stay with at least <b className="reviews__text-amount">50 characters</b>.
+          stay with at least <b className="reviews__text-amount">{MIN_REVIEW_LENGTH} characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button
+          className="reviews__submit form__submit button"
+          type="submit"
+          disabled={!validateForm(reviewData)}
+        >
+          Submit
+        </button>
       </div>
     </form>
   );
