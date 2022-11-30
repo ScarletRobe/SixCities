c33a5deafc0ef311d62b15e0b9d4573aa5190693
Leonid Chernyakov <scrltrb@gmail.com>
2022-11-30 14:30:08 +0300

    Refactor: structure of locations storing | WIP

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,56 @@ export enum AuthorizationStatus {
   Unknown = 'UNKNOWN',
 }
 
-export const LOCATIONS = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
+export const LOCATIONS = [
+  {
+    name: 'Paris',
+    location: {
+      latitude: 48.864716,
+      longitude: 2.349014,
+      zoom: 13
+    }
+  },
+  {
+    name: 'Cologne',
+    location: {
+      latitude: 50.935173,
+      longitude: 6.953101,
+      zoom: 13
+    }
+  },
+  {
+    name: 'Brussels',
+    location: {
+      latitude: 50.846557,
+      longitude: 4.351697,
+      zoom: 13
+    }
+  },
+  {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.377956,
+      longitude: 4.897070,
+      zoom: 13
+    }
+  },
+  {
+    name: 'Hamburg',
+    location: {
+      latitude: 53.551086,
+      longitude: 9.993682,
+      zoom: 13
+    }
+  },
+  {
+    name: 'Dusseldorf',
+    location: {
+      latitude: 51.233334,
+      longitude: 6.783333,
+      zoom: 13
+    }
+  }
+];
 
 export enum CardTypes {
   Main = 'cities',
@@ -29,3 +78,5 @@ export enum Markers {
   Default = 'img/pin.svg',
   Current = 'img/pin-active.svg',
 }
+
+export const defaultCity = LOCATIONS[2];
