56c552e73d24357340967dbb1dec8aeba4f9fec0
Leonid Chernyakov <scrltrb@gmail.com>
2022-11-30 14:33:06 +0300

    Feat: preparation for connecting redux

---
+++ src/store/appSlice.ts
@@ -0,0 +1,42 @@
+import { offers } from './../mocks/offers';
+import { findFavoriteOffers, findOffersByCity } from './../utils';
+import { Offer, City } from './../types/offer';
+import { defaultCity } from '../consts';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type InitialState = {
+  city: City;
+  allOffers: Offer[];
+  offersByCity: Offer[];
+  favoriteOffers: Offer[];
+}
+
+const initialState: InitialState = {
+  city: defaultCity,
+  allOffers: offers,
+  offersByCity: findOffersByCity(defaultCity.name, offers),
+  favoriteOffers: findFavoriteOffers(offers),
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    city: (state, action: PayloadAction<City>) => {
+      state.city = action.payload;
+    },
+    allOffers: (state, action: PayloadAction<Offer[]>) => {
+      state.allOffers = action.payload;
+    },
+    offersByCity: (state, action: PayloadAction<Offer[]>) => {
+      state.offersByCity = action.payload;
+    },
+    favoriteOffers: (state, action: PayloadAction<Offer[]>) => {
+      state.favoriteOffers = action.payload;
+    },
+  },
+});
+
+export const {city, allOffers, offersByCity, favoriteOffers} = appSlice.actions;
+
+export default appSlice.reducer;
