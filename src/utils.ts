672fe4fa83bb74e77f2f94ae7cb4378b5ba89aa2
Leonid Chernyakov <scrltrb@gmail.com>
2022-11-30 14:34:10 +0300

    Feat: functions for searching specific offers

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,10 @@
+import { Offer } from './types/offer';
 export const capitalizeWord = (text: string) => text[0].toUpperCase() + text.slice(1);
+
+export const findOffersByCity = (city: string, offers: Offer[]): Offer[] | [] => (
+  offers.filter((offer) => offer.city.name === city)
+);
+
+export const findFavoriteOffers = (offers: Offer[]) => (
+  offers.filter((offer) => offer.isFavorite)
+);
