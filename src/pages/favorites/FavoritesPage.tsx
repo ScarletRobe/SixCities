96907e85feed8cd05b0841317a7b74b5c752b864
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-03 16:23:08 +0300

    Feat: connect empty favorites card component

--- src/pages/favorites/FavoritesPage.tsx
+++ src/pages/favorites/FavoritesPage.tsx
@@ -1,33 +1,28 @@
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import PlaceCards from '../../components/place-cards/PlaceCards';
-import { CardTypes } from '../../consts';
 import { Offer } from '../../types/offer';
-import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import { Link } from 'react-router-dom';
-import { city } from '../../store/appSlice';
-import { findCityByName } from '../../utils';
+import { useAppSelector } from '../../hooks/redux';
+import FavoritesCard from '../../components/favorites-card/FavoritesCard';
+import EmptyFavoritesCard from '../../components/favorites-card/EmptyFavoritesCard';
 
-type FavoritesOffers = {
+export type FavoritesOffers = {
   [key: string]: Offer[];
 }
 
-function Favorites (): JSX.Element {
-  const favoriteOffers = useAppSelector((state) => state.appReducer.favoriteOffers);
-  const dispatch = useAppDispatch();
+const getFavoritesPlacesFromOffers = (favoriteOffers: Offer[]) => {
+  const result: FavoritesOffers = {};
+  favoriteOffers.forEach((offer) => {
+    if (result?.[offer.city.name]?.length) {
+      result[offer.city.name].push(offer);
+    } else {
+      result[offer.city.name] = [offer];
+    }
+  });
+  return result;
+};
 
-  const getFavoritesPlacesFromOffers = () => {
-    const result: FavoritesOffers = {};
-    favoriteOffers.forEach((offer) => {
-      if (result?.[offer.city.name]?.length) {
-        result[offer.city.name].push(offer);
-      } else {
-        result[offer.city.name] = [offer];
-      }
-    });
-    return result;
-  };
-  const favoritesPlaces = getFavoritesPlacesFromOffers();
+function FavoritesPage(): JSX.Element {
+  const favoriteOffers = useAppSelector((state) => state.appReducer.favoriteOffers);
 
   return (
     <div className="page">
@@ -35,31 +30,11 @@ function Favorites (): JSX.Element {
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-
-              {Object.keys(favoritesPlaces).map((town) => (
-                <li key={town} className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <Link
-                        className="locations__item-link"
-                        to="/"
-                        onClick={() => dispatch(city(findCityByName(town)))}
-                      >
-                        <span>{town}</span>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="favorites__places">
-                    <PlaceCards cardType={CardTypes.Favorites} offers={favoritesPlaces[town]}/>
-                  </div>
-                </li>
-              ))}
-
-            </ul>
-          </section>
+          {
+            favoriteOffers.length
+              ? <FavoritesCard favoritesPlaces={getFavoritesPlacesFromOffers(favoriteOffers)} />
+              : <EmptyFavoritesCard />
+          }
         </div>
       </main>
 
@@ -68,4 +43,4 @@ function Favorites (): JSX.Element {
   );
 }
 
-export default Favorites;
+export default FavoritesPage;
