e60762e3759e893df0268ce4ae84cb92db6d4f46
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-03 16:04:57 +0300

    Feat: function for city searching

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { Offer } from './types/offer';
+import { LOCATIONS } from './consts';
+import { Offer, City } from './types/offer';
 export const capitalizeWord = (text: string) => text[0].toUpperCase() + text.slice(1);
 
 export const findOffersByCity = (city: string, offers: Offer[]): Offer[] | [] => (
@@ -8,3 +9,11 @@ export const findOffersByCity = (city: string, offers: Offer[]): Offer[] | [] =>
 export const findFavoriteOffers = (offers: Offer[]) => (
   offers.filter((offer) => offer.isFavorite)
 );
+
+export const findCityByName = (cityName: string): City => {
+  const city = LOCATIONS.find((loc) => loc.name === cityName);
+  if (!city) {
+    throw new Error('Unknown city name');
+  }
+  return city;
+};
