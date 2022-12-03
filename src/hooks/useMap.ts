f1e4f3545089e9b6eedfb0e0daf0e894e9773250
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-03 19:33:02 +0300

    Refactor: change zoom with static number

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -6,6 +6,8 @@ import { Location } from '../types/offer';
 import { TileLayerConfig } from '../consts';
 import L from 'leaflet';
 
+const DEFAULT_ZOOM = 13;
+
 function useMap(
   mapRef: MutableRefObject<HTMLEmbedElement | null>,
   location: Location,
@@ -15,7 +17,7 @@ function useMap(
 
   useEffect(() => {
     if (map) {
-      map.setView(new L.LatLng(location.latitude, location.longitude), location.zoom);
+      map.setView(new L.LatLng(location.latitude, location.longitude), DEFAULT_ZOOM);
     }
     if (mapRef.current !== null && !isRenderedRef.current) {
       const instance = new Map(mapRef.current, {
