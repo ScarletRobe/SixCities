fdde9d71bc2f0fb8cdfc6a452ff7f460ca82589a
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-05 17:56:35 +0300

    Feat: auth status action

--- src/store/appSlice.ts
+++ src/store/appSlice.ts
@@ -1,4 +1,5 @@
-import { offers } from './../mocks/offers';
+import { AuthorizationStatus } from './../consts';
+// import { offers } from './../mocks/offers';
 import { findFavoriteOffers, findOffersByCity } from './../utils';
 import { Offer, City } from './../types/offer';
 import { defaultCity } from '../consts';
@@ -9,13 +10,15 @@ type InitialState = {
   allOffers: Offer[];
   offersByCity: Offer[];
   favoriteOffers: Offer[];
+  authorizationStatus: AuthorizationStatus;
 }
 
 const initialState: InitialState = {
   city: defaultCity,
-  allOffers: offers,
-  offersByCity: findOffersByCity(defaultCity.name, offers),
-  favoriteOffers: findFavoriteOffers(offers),
+  allOffers: [],
+  offersByCity: findOffersByCity(defaultCity.name, []),
+  favoriteOffers: findFavoriteOffers([]),
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const appSlice = createSlice({
@@ -24,7 +27,7 @@ export const appSlice = createSlice({
   reducers: {
     city: (state, action: PayloadAction<City>) => {
       state.city = action.payload;
-      state.offersByCity = findOffersByCity(action.payload.name, offers);
+      state.offersByCity = findOffersByCity(action.payload.name, state.allOffers);
     },
     allOffers: (state, action: PayloadAction<Offer[]>) => {
       state.allOffers = action.payload;
@@ -35,9 +38,12 @@ export const appSlice = createSlice({
     favoriteOffers: (state, action: PayloadAction<Offer[]>) => {
       state.favoriteOffers = action.payload;
     },
+    authorizationStatus: (state, action: PayloadAction<AuthorizationStatus>) => {
+      state.authorizationStatus = action.payload;
+    }
   },
 });
 
-export const {city, allOffers, offersByCity, favoriteOffers} = appSlice.actions;
+export const {city, allOffers, offersByCity, favoriteOffers, authorizationStatus} = appSlice.actions;
 
 export default appSlice.reducer;
