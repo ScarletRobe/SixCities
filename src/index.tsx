66a24ec638bfcfaee1dd879dd51b121ca6d28364
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-05 22:14:36 +0300

    Feat: fetch offers when app starts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { comments } from './mocks/comments';
+import { fetchOffers } from './store/apiActions';
 import { store } from './store/store';
+
+
+store.dispatch(fetchOffers());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
