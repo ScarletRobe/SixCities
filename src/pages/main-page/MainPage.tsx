66fa6a8bbbae195ac7818f3ce8ad689b7ea68098
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-06 15:48:53 +0300

    Feat: loader, temp error message

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,11 +1,28 @@
+/* eslint-disable no-nested-ternary */
 import Header from '../../components/header/Header';
 import Locations from '../../components/locations/Locations';
 import EmptyOffersSection from '../../components/offersSection/EmptyOffersSection';
 import OffersSection from '../../components/offersSection/OffersSection';
+import OffersSectionLoading from '../../components/offersSection/OffersSectionLoading';
 import { useAppSelector } from '../../hooks/redux';
+import { Offer } from '../../types/offer';
+
+const getOffersSectionElement = (offersByCity: Offer[], isLoading: boolean, loadingError: string | null) => {
+  if (isLoading) {
+    return <OffersSectionLoading />;
+  }
+  if (loadingError) {
+    return `${loadingError}`;
+  }
+  if (!offersByCity.length) {
+    return <EmptyOffersSection />;
+  } else {
+    return <OffersSection />;
+  }
+};
 
 function MainPage(): JSX.Element {
-  const currentCityOffers = useAppSelector((state) => state.appReducer.offersByCity);
+  const {offersByCity, isLoading, loadingError} = useAppSelector((state) => state.appReducer);
 
   return (
     <div className="page page--gray page--main">
@@ -15,17 +32,13 @@ function MainPage(): JSX.Element {
       />
 
       <main
-        className={`page__main page__main--index ${currentCityOffers.length ? '' : 'page__main--index-empty'}`}
+        className={`page__main page__main--index ${offersByCity.length ? '' : 'page__main--index-empty'}`}
       >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <Locations />
         </div>
-        {
-          currentCityOffers.length
-            ? <OffersSection />
-            : <EmptyOffersSection />
-        }
+        {getOffersSectionElement(offersByCity, isLoading, loadingError)}
       </main>
     </div>
   );
