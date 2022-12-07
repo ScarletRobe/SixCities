13c3609576e8bb0bd90aaccd713566b2bd70515e
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-07 16:11:59 +0300

    Feat: init components

---
+++ src/components/header/AuthNavList.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { logout } from '../../store/apiActions';
+
+function AuthNavList () {
+  const dispatch = useAppDispatch();
+  const favoriteOffers = useAppSelector((state) => state.appReducer.favoriteOffers);
+
+  return (
+    <ul className="header__nav-list">
+      <li className="header__nav-item user">
+        <Link className="header__nav-link header__nav-link--profile" to="/favorites">
+          <div className="header__avatar-wrapper user__avatar-wrapper">
+          </div>
+          <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+          <span className="header__favorite-count">{favoriteOffers.length}</span>
+        </Link>
+      </li>
+      <li className="header__nav-item">
+        <div className="header__nav-link">
+          <span
+            className="header__signout"
+            onClick={() => {
+              dispatch(logout());
+            }}
+          >
+              Sign out
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default AuthNavList;
