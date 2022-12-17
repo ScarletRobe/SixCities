5433c63e163093c77bf8f87c116dd33a525f5eec
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-17 20:51:02 +0300

    Refactor: decompose

--- src/components/offers-section/OffersSectionLoading.tsx
+++ src/components/offers-section/OffersSectionLoading.tsx
@@ -1,17 +1,11 @@
-import classes from './offersSection.module.css';
+import Spinner from '../UI/spinner/Spinner';
 
 function OffersSectionLoading () {
   return (
     <div className="cities">
       <div className="cities__places-container cities__places-container--empty container">
         <section className="cities__no-places">
-          <div className={classes.citiesStatusWrapper}>
-            <div className={classes.loaderWrapper}>
-              <div className={classes.loader}>
-                <div></div>
-              </div>
-            </div>
-          </div>
+          <Spinner />
         </section>
         <div className="cities__right-section"></div>
       </div>
