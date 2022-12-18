786ac3de183287805e4f80c5da625501a2727b64
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-18 15:03:42 +0300

    Feat: loading page component

---
+++ src/pages/loading-page/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import Spinner from '../../components/UI/spinner/Spinner';
+
+function LoadingPage () {
+  return (
+    <main className='page page--loading'>
+      <Spinner />
+    </main>
+  );
+}
+
+export default LoadingPage;
