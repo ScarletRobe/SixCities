523f324acdee580586ea19dd55bcb7f7b106c930
Leonid Chernyakov <scrltrb@gmail.com>
2022-12-18 15:04:16 +0300

    Fix: redirect to login page before checking auth

--- src/components/private-route/PrivateRoute.tsx
+++ src/components/private-route/PrivateRoute.tsx
@@ -1,18 +1,27 @@
 import {Navigate} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../consts';
+import LoadingPage from '../../pages/loading-page/LoadingPage';
 
 type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
   children: JSX.Element;
 }
+const renderSwitch = (authorizationStatus: AuthorizationStatus, children: JSX.Element) => {
+  switch (authorizationStatus) {
+    case AuthorizationStatus.Unknown:
+      return <LoadingPage />;
+    case AuthorizationStatus.Auth:
+      return children;
+    case AuthorizationStatus.NoAuth:
+      return <Navigate to={AppRoute.Login} />;
+  }
+};
 
 function PrivateRoute(props: PrivateRouteProps): JSX.Element {
   const {authorizationStatus, children} = props;
 
   return (
-    authorizationStatus === AuthorizationStatus.Auth
-      ? children
-      : <Navigate to={AppRoute.Login} />
+    renderSwitch(authorizationStatus, children)
   );
 }
 
